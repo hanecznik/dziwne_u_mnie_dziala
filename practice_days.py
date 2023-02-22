1b1a0a847be14a39058d7fc294c59632058e784e
krzysztof <krzysztof.szymczyk@yahoo.com>
2023-02-22 15:27:26 +0100

    fix for DeprecationWarning

--- practice_days.py
+++ practice_days.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(executable_path="C:/Users/grapefruit/Desktop/browserdrivers/chromedriver_mac64")
+s = Service('C:/Users/grapefruit/Desktop/browserdrivers/chromedriver_mac64')
+driver = webdriver.Chrome(service=s)
 driver.get("https://www.google.pl")
 
-
-
-#
 # # driver = webdriver.Chrome (ChromeDriverManager().install())
 # driver.get("https://www.tvn24.pl")
 # driver.maximize_window()
@@ -16,4 +15,3 @@ driver.get("https://www.google.pl")
 # # driver.find_elements("//*[@id="__next"]/div/header/nav/ul/li[2]/ul/li[3]/div/div[1]/button/svg/path")
 # driver.close()
 #
-
