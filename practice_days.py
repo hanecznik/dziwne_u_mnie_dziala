18f157841995348229e6b01b173fac16f106a99a
Ania Palka <grapefruit@MacBook-Air-Ania.local>
2023-02-22 15:02:17 +0100

    practice_days

---
+++ practice_days.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:/Users/grapefruit/Desktop/browserdrivers/chromedriver_mac64")
+driver.get("https://www.google.pl")
+
+
+
+#
+# # driver = webdriver.Chrome (ChromeDriverManager().install())
+# driver.get("https://www.tvn24.pl")
+# driver.maximize_window()
+# element=WebDriverWait(driver,30)
+# # drive.manage().timeouts()implicitlyWait(10, TimeUnit.SECONDS)
+# driver.find_element("onetrust-accept-btn-handler").click()
+# element=WebDriverWait(driver,15)
+# # driver.find_elements("//*[@id="__next"]/div/header/nav/ul/li[2]/ul/li[3]/div/div[1]/button/svg/path")
+# driver.close()
+#
+
