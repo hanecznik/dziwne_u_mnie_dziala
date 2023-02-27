aacf393d40ea8cf995cab1bc79100ae3d39b7f85
Ania Palka <grapefruit@MacBook-Air-Ania.local>
2023-02-27 14:23:56 +0100

    test-sigma.py

--- test-sigma.py
+++ test-sigma.py
@@ -2,17 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-import pyautogui
 import time
 
 driver.get("https://travel.testsigma.com/")
-driver.maximize.window()
-time.sleep(7)
-myPageTitle=driver.title
-print(myPageTitle)
-time.sleep(4)
-print("Printed after 4")
-def screenshot():
-    image = pyautogui.screenshot()
-    screenshot.save ('picture_from_pycharm.png')
+driver.maximize_window()
+time.sleep(5)
+x = driver.find_element(By.XPATH, //*[@id="dropdownMenuButton"])
+x.click()
+y = driver.find_element(By.CLASS_NAME = "text-muted text-center")
+y.click()
 driver.close()
