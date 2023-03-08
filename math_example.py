56f24e37f7550c6d8418703afbcaa7747e4051b2
krzysztof <krzysztof.szymczyk@yahoo.com>
2023-03-08 16:59:00 +0100

    small fix

--- math_example.py
+++ math_example.py
@@ -6,17 +6,15 @@ a = float(input("podaj wartość parametru a: "))
 b = float(input("podaj wartość parametru b: "))
 c = float(input("podaj wartość parametru c: "))
 
-delta = (b**2)-(4*a*c)
+delta = (b ** 2) - (4 * a * c)
 
 if delta > 0:
-    x1 = (-b - math.sqrt(delta))/(2*a)
-    x2 = (-b + math.sqrt(delta))/(2*a)
-    print ('x1 = ', x1)
-    print ('x2 = ', x2)
-
+    x1 = (-b - math.sqrt(delta)) / (2 * a)
+    x2 = (-b + math.sqrt(delta)) / (2 * a)
+    print('x1 = ', x1)
+    print('x2 = ', x2)
 elif delta == 0:
-    x = -b/(2*a)
+    x = -b / (2 * a)
     print('x0 = ', x)
 else:
     print("nie liczymy miejsc zerowych")
-
