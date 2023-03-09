bc00d90af018ec2e702647704d5b2c36689584b7
krzysztof <krzysztof.szymczyk@yahoo.com>
2023-03-09 17:13:18 +0100

    added parallel execution

--- sample.py
+++ sample.py
@@ -3,7 +3,6 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
 from Pages.main_page import MainPage
 
@@ -18,6 +17,20 @@ def browser():
 
 
 def test_check_amount_of_points_by_club_name(browser):
+    tvn_sport_page = MainPage.navigate(browser, 'https://eurosport.tvn24.pl')
+    title = tvn_sport_page.get_title()
+    assert title == 'Sport, Wyniki meczów, Wiadomości sportowe | Eurosport w TVN24'
+    points = tvn_sport_page.get_amount_of_points_by_club_name('Legia')
+    assert points == '46'
+    footbal_page = tvn_sport_page.go_to_football_page()
+    title = footbal_page.get_title()
+    assert title == 'Piłka nożna | Eurosport w TVN24'
+    meteo_page = footbal_page.go_to_meteo_page()
+    title = meteo_page.get_title()
+    assert title == 'Prognoza pogody i wiadomości pogodowe w TVN Meteo - TVN Meteo'
+
+
+def test_check_amount_of_points_by_club_name2(browser):
     tvn_sport_page = MainPage.navigate(browser, 'https://eurosport.tvn24.pl')
     title = tvn_sport_page.get_title()
     assert title == 'Sport, Wyniki meczów, Wiadomości sportowe | Eurosport w TVN24'
